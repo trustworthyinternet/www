0fa3f2c5f7243abebc9c739812f73467a2390511
Keith Shaw <kshaw@qualys.com>
2012-02-28 05:51:46 +0000

    Trustworthy Internet: * added better default 404 and 403 pages

---
+++ 404_notfound.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Page not found | Trustworthy Internet Movement</title>
+	<link rel="stylesheet" type="text/css" href="/asset/stylesheet/main.css" />
+	<!--[if IE]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body id="home">
+	<h1>Page not found</h1>
+	<p>
+		Please try visiting the<br />
+		<a href="/">Trustworthy Internet Movement</a><br />
+		home page.
+	</p>
+</body>
+</html>
